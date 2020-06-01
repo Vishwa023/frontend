df5e3ab58fc78a02f573c13b3dd34cd506f160d5
vishwa23 <vishwa.patel@treebohotels.com>
2020-06-01 17:10:58 +0530

    Adding jQery examples

---
+++ JavaScript/Basics/jQuery/exe.js
@@ -0,0 +1,6 @@
+$("div").css("color","red");
+$("div").css("background","blue");
+$(".highlight").css("width","200px");
+$("#third").css("border","2px solid black");
+$("div:first-of-type").css("color","black");
+
